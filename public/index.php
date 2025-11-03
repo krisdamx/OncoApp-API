5d0bd89c577f791f4d55b81e5a9a84362ac51e3a
Francisco Silva <fsilvalagos@gmail.com>
2025-11-03 15:53:00 -0600

    fuerza formato JSON en todas las respuestas y errores

--- public/index.php
+++ public/index.php
@@ -18,6 +18,20 @@ AppFactory::setContainer($containerBuilder->build());
 AppFactory::setResponseFactory(new \Nyholm\Psr7\Factory\Psr17Factory());
 $app = AppFactory::create();
 
+$app->add(function ($request, $handler) {
+    $response = $handler->handle($request);
+    $path = $request->getUri()->getPath();
+
+    if (str_starts_with($path, '/api/')) {
+        $ct = $response->getHeaderLine('Content-Type');
+        if ($ct === '' || stripos($ct, 'application/json') === false) {
+            $response = $response->withHeader('Content-Type', 'application/json; charset=utf-8');
+        }
+    }
+    return $response;
+});
+
+
 // Middlewares
 $app->addBodyParsingMiddleware();
 $app->addRoutingMiddleware();
@@ -27,7 +41,32 @@ $app->options('/{routes:.+}', function ($request, $response) {
     return $response;
 });
 
-$errorMiddleware = $app->addErrorMiddleware(true, false, false);
+$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+
+$customErrorHandler = function ($request, $exception, $displayErrorDetails) use ($app) {
+    $status = 500;
+    $title  = 'Internal Server Error';
+    if ($exception instanceof \Slim\Exception\HttpException) {
+        $status = $exception->getCode() ?: 500;
+        $title  = $exception->getTitle();
+    }
+
+    $payload = [
+        'error' => [
+            'status' => $status,
+            'title'  => $title,
+            'detail' => $displayErrorDetails ? $exception->getMessage() : null,
+            'path'   => (string)$request->getUri()->getPath(),
+        ]
+    ];
+
+    $response = $app->getResponseFactory()->createResponse($status);
+    $response->getBody()->write(json_encode($payload, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES));
+
+    return $response->withHeader('Content-Type', 'application/json; charset=utf-8');
+};
+
+$errorMiddleware->setDefaultErrorHandler($customErrorHandler);
 // (displayErrorDetails, logErrors, logErrorDetails)
 // pon true solo en desarrollo si de verdad quieres stacktraces en pantalla
 
